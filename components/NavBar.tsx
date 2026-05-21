bb2c2af43fc68afa6158ff484f2ff3943c615ee0
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2026-05-21 17:05:10 -0300

    add some colors and navBar layout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,17 +1,20 @@
 export default function NavBar() {
     return (
-        <nav className="px-8 py-4">
-            <ul className="flex gap-8">
-                <li className="font-bold">
-                    Home
-                </li>
-                <li className="ml-auto">
-                    Sobre
-                </li>
-                <li>
-                    Contato
-                </li>
-            </ul>
+        <nav className="bg-primary-red border-b-3 border-accent h-20">
+            <div className="flex h-full items-center justify-between px-8 text-background">
+                <ul className="flex gap-8 w-full">
+                    <li className="font-bold">
+                        Home
+                    </li>
+
+                    <li className="ml-auto">
+                        Sobre
+                    </li>
+                    <li>
+                        Contato
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
