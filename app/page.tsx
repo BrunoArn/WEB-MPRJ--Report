3b6acaeee6be42b57b7241b4e3d4afa9b6560714
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2026-05-21 15:43:11 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div>
+      <main className="flex h-screen items-center justify-center">
+        <h1 className="text-6xl font-bold text-center">
+          Ministério público do Rio de Janeiro
+        </h1>
+      </main>
+    </div>
+  );
+}
